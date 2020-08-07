615f9732774225871befa03156f35028a2ea600f
Redwan Newaz <redwan06me@gmail.com>
2020-08-07 15:00:57 -0400

    help updated

--- main.cpp
+++ main.cpp
@@ -4,20 +4,26 @@
 int main(int argc, char *argv[]) {
 
     std::cout << "Resizer Initilize" << std::endl;
-    ArgParser argParser;
-    OPTION opt = argParser.parse_args(argc, argv);
+
     options_description desc{"Options"};
     desc.add_options()
             ("help,h", "Help screen")
-            ("size", value<std::string>(), "size =(widthxheight)")
-            ("crop",value<std::string>(), "rect =(originXxoriginYxwidthxheight")
+            ("size", value<std::string>(), "size =(width x height)")
+            ("crop",value<std::string>(), "rect =(originX x originY x width x height)")
             ("dest", value<std::string>()->default_value("./"), "destination folder")
             ("conf", value<std::string>(), "JSON configuration file");
 
     std::shared_ptr<variables_map> vm = std::make_shared<variables_map>();
     store(parse_command_line(argc, argv, desc), *vm);
     notify(*vm);
+    if (vm->count("help")) {
+        std::cout << desc << "\n";
+        return 1;
+    }
 
+
+    ArgParser argParser;
+    OPTION opt = argParser.parse_args(argc, argv);
     ImgResizer resizer(vm);
 
     switch (opt) {
